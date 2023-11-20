0399212b22f8c642337635ba9a36c4730b780e87
yjin-jo <dpwls5100@icloud.com>
2023-11-20 13:03:58 +0900

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "FinalReport_02_20210814"
+include(":app")
+ 
