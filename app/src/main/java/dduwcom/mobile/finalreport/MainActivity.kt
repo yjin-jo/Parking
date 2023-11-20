fd89bb6f5e1f2176077d445a6cc2c4bec2ecdf2e
yjin-jo <dpwls5100@icloud.com>
2023-11-20 13:18:10 +0900

    [add/testCode] - MainActivity에 TestCode Log작성

--- app/src/main/java/dduwcom/mobile/finalreport/MainActivity.kt
+++ app/src/main/java/dduwcom/mobile/finalreport/MainActivity.kt
@@ -1,11 +1,14 @@
 package dduwcom.mobile.finalreport
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "pull test")
     }
 }
