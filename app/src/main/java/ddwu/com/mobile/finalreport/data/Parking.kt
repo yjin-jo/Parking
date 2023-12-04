a08e77bdc15fd39226dcd0538f8345befa4bff40
yjin-jo <dpwls5100@icloud.com>
2023-12-04 15:15:32 +0900

    [feat] : parking dto 수정 #1

--- app/src/main/java/ddwu/com/mobile/finalreport/data/Parking.kt
+++ app/src/main/java/ddwu/com/mobile/finalreport/data/Parking.kt
@@ -2,36 +2,44 @@ package ddwu.com.mobile.finalreport.data
 
 import com.google.gson.annotations.SerializedName
 
-data class ParkingRoot(val getparkinginfo: Getparkinginfo,)
-data class Getparkinginfo(
+data class ParkingRoot(
+    @SerializedName("GetParkingInfo")
+    val getParkingInfo: GetparkingInfo,)
+data class GetparkingInfo(
+    @SerializedName("list_total_count")
+    val listTotalCount: Long,
     @SerializedName("row")
     val parkings: List<Parking>,
 )
 data class Parking(
-    @SerializedName("parking_name")
-    val parkingName: String,
-    val addr: String,
-    val tel: String,
-    val capacity: Long,
-    val rates: Long,
-    @SerializedName("cur_parking")
-    val curParking: Long,
-    @SerializedName("time_rate")
-    val timeRate: Long,
-    @SerializedName("add_rates")
-    val addRates: Long,
-    @SerializedName("add_time_rate")
-    val addTimeRate: Long,
-    @SerializedName("weekday_begin_time")
-    val weekdayBeginTime: Long,
-    @SerializedName("weekday_end_time")
-    val weekdayEndTime: Long,
-    @SerializedName("weekend_begin_time")
-    val weekendBeginTime: Long,
-    @SerializedName("weekend_end_time")
-    val weekendEndTime: Long,
-    @SerializedName("holiday_begin_time")
-    val holidayBeginTime: Long,
-    @SerializedName("holiday_end_time")
-    val holidayEndTime: Long,
+    @SerializedName("PARKING_NAME")
+    var parkingName: String,
+    @SerializedName("ADDR")
+    var addr: String,
+    @SerializedName("TEL")
+    var tel: String,
+    @SerializedName("CAPACITY")
+    var capacity: Long,
+    @SerializedName("RATES")
+    var rates: Long,
+    @SerializedName("CUR_PARKING")
+    var curParking: Long,
+    @SerializedName("TIME_RATE")
+    var timeRate: Long,
+    @SerializedName("ADD_RATES")
+    var addRates: Long,
+    @SerializedName("ADD_TIME_RATE")
+    var addTimeRate: Long,
+    @SerializedName("WEEKDAY_BEGIN_TIME")
+    var weekdayBeginTime: Long,
+    @SerializedName("WEEKDAY_END_TIME")
+    var weekdayEndTime: Long,
+    @SerializedName("WEEKEND_BEGIN_TIME")
+    var weekendBeginTime: Long,
+    @SerializedName("WEEKEND_END_TIME")
+    var weekendEndTime: Long,
+    @SerializedName("HOLIDAY_BEGIN_TIME")
+    var holidayBeginTime: Long,
+    @SerializedName("HOLIDAY_END_TIME")
+    var holidayEndTime: Long
 )
