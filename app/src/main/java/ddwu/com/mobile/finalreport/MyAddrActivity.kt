11be99124bfcd104e183b3864fac33dda77f29fd
yjin-jo <dpwls5100@icloud.com>
2023-12-09 14:05:13 +0900

    [add] : MyAddrActivity 추가 #1

---
+++ app/src/main/java/ddwu/com/mobile/finalreport/MyAddrActivity.kt
@@ -0,0 +1,11 @@
+package ddwu.com.mobile.finalreport
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyAddrActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_addr)
+    }
+}
